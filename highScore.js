3d8e61c589426ffbf558c23a1c4aea8fe7cf336b
iddrisu sumaila <sumaila129@gmail.com>
2020-10-06 20:31:49 -0700

    success implemented the fetch appi with a locale json file not proceeding to using fetct with trivia database

--- highScore.js
+++ highScore.js
@@ -3,5 +3,11 @@ const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 
 highScoreList.innerHTML = highScores.map(score =>{
- return `<li class="high-score">${score.name}  -  ${score.score}</li>`;
+//  return `<li class="high-score">${score.name}  -  ${score.score}</li>`;
+return`
+  <tr>
+    <td>${score.name}</td>
+    <td>${score.score}</td>
+  </tr>
+`;
 }).join("");
